eda79ac31bfb04e551dfe39b6de339f581898658
fedeoss <federica.zani2003@gmail.com>
2021-02-15 13:07:18 +0100

    cambio immagini in stile barbie

--- ascensore/ascensore/obj/Debug/ascensore_Content.g.i.cs
+++ ascensore/ascensore/obj/Debug/ascensore_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("bott.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("barbie.png")]
 
 
